e2a0e5398eca03697caf19d65a28e622ee6a0066
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-02 14:54:45 +0100

    add header content

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -1,5 +1,70 @@
-function Header() {
-  return <h1>Footer</h1>;
-}
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-primary position-relative main-header">
+        <div className="container-fluid">
+          <Link className="secondary-font fs-1-25rem-to-3-25rem dark-gray-font" to={"/"}>
+            thbergseng
+          </Link>
+          <button className="btn btn-primary d-lg-none" onClick={handleShow}></button>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to={"#about"} className="btn main-font fs-1-5rem">
+                Login
+              </Link>
+              <Link to={"#resume"} className="btn main-font fs-1-5rem">
+                About me
+              </Link>
+              <Link to={"projects"} className="btn main-font fs-1-5rem">
+                Resume
+              </Link>
+              <Link to={"#services"} className="btn main-font fs-1-5rem">
+                Services
+              </Link>
+              <Link to={"#contact"} className="btn main-font fs-1-5rem">
+                Contact
+              </Link>
+              <Link to={"/login"} className="btn main-font fs-1-5rem">
+                Flag
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+      <Offcanvas className="gray-color d-block d-lg-none" show={show} onHide={handleClose} responsive="lg">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title className="gray-color secondary-font fs-1-5rem">thbergseng</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body className="d-flex flex-column gray-color align-items-start">
+          <Link to={"/"} className="btn main-font fs-1-25rem">
+            About me
+          </Link>
+          <Link to={"/"} className="btn main-font fs-1-25rem">
+            Resume
+          </Link>
+          <Link to={"/"} className="btn main-font fs-1-25rem">
+            Services
+          </Link>
+          <Link to={"/"} className="btn main-font fs-1-25rem">
+            Contact
+          </Link>
+          <Link to={"/"} className="btn main-font fs-1-25rem">
+            Flag
+          </Link>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};
 
 export default Header;
