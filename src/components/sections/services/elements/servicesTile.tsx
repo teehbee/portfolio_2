b1bcb7c75235cc93670b4dc18e4c936c0f51d64b
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-04 09:56:15 +0100

    finish adding text to services section

--- src/components/sections/services/elements/servicesTile.tsx
+++ src/components/sections/services/elements/servicesTile.tsx
@@ -7,7 +7,7 @@ const ServicesTile: React.FC<servicesTileInterface> = ({ servicesIcon, servicesT
   return (
     <div className="services-tile d-flex flex-column align-items-center justify-content-center">
       <img className="services-icon" src={servicesIcon} aria-label="service icon" />
-      <p className="mb-0 dark-gray-font fs-0-75rem-to-1-25rem">{servicesTitle}</p>
+      <p className="mb-0 dark-gray-font fs-0-75rem-to-1-25rem fw-medium">{servicesTitle}</p>
     </div>
   );
 };
--- src/components/sections/services/servicesSection.tsx
+++ src/components/sections/services/servicesSection.tsx
@@ -7,6 +7,9 @@ function ServicesSection() {
       <div className="text-start">
         <h3 className="my-5 fs-2rem-to-3rem">Services</h3>
       </div>
+      <div className="text-center text-lg-start my-5">
+        <p className="yellow-font fw-bold fs-1rem-to-1-5rem">These are some of the services I can offer. Feel free to contact me if these suit your needs.</p>
+      </div>
       <div className="text-center d-flex justify-content-center justify-content-lg-start flex-row gap-3">
         <ServicesTile servicesIcon={codeIcon} servicesTitle="Web-design" />
         <ServicesTile servicesIcon={translationIcon} servicesTitle="Translations" />
