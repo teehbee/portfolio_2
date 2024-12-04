f2365276cd660096bdb110b9f10d560a31e16b96
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-04 16:38:31 +0100

    styling corrections to contact section

--- src/components/sections/contact/elements/ContactInformation.tsx
+++ src/components/sections/contact/elements/ContactInformation.tsx
@@ -1,12 +1,12 @@
 import { contactAtIcon, contactPhoneIcon, contactLocationIcon, contactGitHubIcon, contactLinkedInIcon } from "../../../../assets/icons";
 import ContactBubble from "./contactBubble";
-import { mapsUrl } from "../../../data";
+import { mapsUrl, email, phone } from "../../../data";
 
 function ContactInfo() {
   return (
-    <div className="col-12 col-md-7  mt-4 mt-lg-0 d-flex flex-column justify-content-between border py-lg-5">
-      <ContactBubble icon={contactAtIcon} description="contact via email" href="mailto:example@gmail.com" linkText="thbergseng@gmail.com" />
-      <ContactBubble icon={contactPhoneIcon} description="contact via email" href="tel:+47148822" linkText="+4747148822" />
+    <div className="col-12 col-md-7  mt-4 mt-lg-0 d-flex flex-column justify-content-between py-lg-5 ps-lg-4">
+      <ContactBubble icon={contactAtIcon} description="contact via email" href={email} linkText="thbergseng@gmail.com" />
+      <ContactBubble icon={contactPhoneIcon} description="contact via email" href="tel:+47148822" linkText={phone} />
       <ContactBubble icon={contactLocationIcon} description="contact via email" href={mapsUrl} linkText="thbergseng@gmail.com" />
       <div className="d-flex">
         <ContactBubble icon={contactGitHubIcon} description="Link to github profile" href="https://github.com/teehbee" linkText="" />
