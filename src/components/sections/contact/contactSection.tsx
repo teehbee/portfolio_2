5fdabf1af66ba803c64083476952b8bb4c795196
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-04 10:47:54 +0100

    install react-hook-forms and yup

--- src/components/sections/contact/contactSection.tsx
+++ src/components/sections/contact/contactSection.tsx
@@ -1,5 +1,15 @@
 function ContactSection() {
-  return <h1>Contact</h1>;
+  return (
+    <section id="#projects-section" className="container text-center section-border">
+      <div className="text-center text-lg-start">
+        <h3 className="my-5 fs-2rem-to-3rem">Contact</h3>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-5 border">Test</div>
+        <div className="col-12 col-md-7 border">Test</div>
+      </div>
+    </section>
+  );
 }
 
 export default ContactSection;
