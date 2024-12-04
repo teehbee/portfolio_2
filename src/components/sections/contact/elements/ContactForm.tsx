49675fcde1623fcf85b378861bec14287814dec0
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-04 10:58:49 +0100

    add inputs and lables to form

--- src/components/sections/contact/elements/ContactForm.tsx
+++ src/components/sections/contact/elements/ContactForm.tsx
@@ -1,5 +1,27 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 function ContactForm() {
-  return <div className="col-12 col-md-5 border">Contact form goes here</div>;
+  return (
+    <div className="col-12 col-md-5 border">
+      <form action="">
+        <div className="d-flex flex-column text-start">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder="Enter full name here" />
+        </div>
+        <div className="d-flex flex-column text-start">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="Enter email address here" />
+        </div>
+        <div className="d-flex flex-column text-start">
+          <label htmlFor="message">Message</label>
+          <textarea id="message" placeholder="Type your message" />
+        </div>
+        <button className="w-100 mt-3">Send message</button>
+      </form>
+    </div>
+  );
 }
 
 export default ContactForm;
