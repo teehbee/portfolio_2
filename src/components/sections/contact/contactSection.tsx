6c7732b462e5edc550b1561b27c9bd4f06b4fe6b
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-04 16:38:57 +0100

    add margin-bottom to contact section

--- src/components/sections/contact/contactSection.tsx
+++ src/components/sections/contact/contactSection.tsx
@@ -2,7 +2,7 @@ import { ContactForm, ContactInfo } from "./elements";
 
 function ContactSection() {
   return (
-    <section id="#projects-section" className="container text-center section-border">
+    <section id="#projects-section" className="container text-center section-border mb-5">
       <div className="text-center text-lg-start">
         <h3 className="my-5 fs-2rem-to-3rem">Contact</h3>
       </div>
