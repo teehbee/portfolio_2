91d9cdc076acc06308ad32a452d600fdc4681740
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-04 15:51:10 +0100

    add styling to labels and input for contact form

--- src/components/sections/contact/elements/ContactForm.tsx
+++ src/components/sections/contact/elements/ContactForm.tsx
@@ -31,18 +31,24 @@ function ContactForm() {
     <div className="col-12 col-md-5 border">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="d-flex flex-column text-start">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="Enter full name here" {...register("name")} />
+          <label className="mt-3" htmlFor="name">
+            Name
+          </label>
+          <input className="contact-input" type="text" id="name" placeholder="Enter full name here" {...register("name")} />
           {errors.name && <p>errors.name.message</p>}
         </div>
         <div className="d-flex flex-column text-start">
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder="Enter email address here" {...register("email")} />
+          <label className="mt-3" htmlFor="email">
+            Email
+          </label>
+          <input className="contact-input" type="text" id="email" placeholder="Enter email address here" {...register("email")} />
           {errors.email && <p>{errors.email.message}</p>}
         </div>
         <div className="d-flex flex-column text-start">
-          <label htmlFor="message">Message</label>
-          <textarea id="message" placeholder="Type your message" {...register("message")} />
+          <label className="mt-3" htmlFor="message">
+            Message
+          </label>
+          <textarea className="contact-input" id="message" placeholder="Type your message" {...register("message")} />
           {errors.message && <p>{errors.message.message}</p>}
         </div>
         <button className="w-100 mt-3">Send message</button>
