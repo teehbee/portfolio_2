e57ff4634971fa7169cd37baac5c61f061d34a02
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-10 09:33:20 +0100

    add content to studies section in resume

--- src/components/sections/resume/elements/Studies.tsx
+++ src/components/sections/resume/elements/Studies.tsx
@@ -5,9 +5,10 @@ function Studies() {
   return (
     <div className="col-12 col-lg-7">
       <h4 className="yellow-font fs-1-5rem-to-2rem my-5">Education</h4>
-      <ResumeTile resumeIcon={studentIcon} fromYear={"2022"} toYear={"2022"} nameOfStudyorPosition={"study program"} nameOfSchoolOrEmployer={"Name of school"} />
-      <ResumeTile resumeIcon={studentIcon} fromYear={"2022"} toYear={"2022"} nameOfStudyorPosition={"study program"} nameOfSchoolOrEmployer={"Name of school"} />
-      <ResumeTile resumeIcon={studentIcon} fromYear={"2022"} toYear={"2022"} nameOfStudyorPosition={"study program"} nameOfSchoolOrEmployer={"Name of school"} />
+      <ResumeTile resumeIcon={studentIcon} fromYear={"January 2022"} toYear={"January 2024"} nameOfStudyorPosition={"Front-end development"} nameOfSchoolOrEmployer={"Noroff school of technology and digital media, online"} />
+      <ResumeTile resumeIcon={studentIcon} fromYear={"August 2009"} toYear={"July 2010"} nameOfStudyorPosition={"Supplementary general studies"} nameOfSchoolOrEmployer={"Nes secondary school"} />
+      <ResumeTile resumeIcon={studentIcon} fromYear={"August 2006"} toYear={"July 2007"} nameOfStudyorPosition={"Maritime studies"} nameOfSchoolOrEmployer={"Etterstad secondary school"} />
+      <ResumeTile resumeIcon={studentIcon} fromYear={"August 2005"} toYear={"July 2006"} nameOfStudyorPosition={"Electrician studies"} nameOfSchoolOrEmployer={"Nes secondary school"} />
     </div>
   );
 }
