ffca9cbea619c5f21f0a1bf9d195db3eda80665d
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-12 15:48:14 +0100

    add about text in english

--- src/components/sections/about/aboutSection.tsx
+++ src/components/sections/about/aboutSection.tsx
@@ -9,11 +9,8 @@ function AboutSection() {
         </div>
         <div className="col-12 col-lg-8 text-center text-lg-start pe-lg-5">
           <h3 className="mb-4 mt-4 mt-lg-0 fs-2rem-to-3rem">About me</h3>
-          <p className="yellow-font fs-1rem-to-1-25rem">Small introduction text about me. Small introduction text about me. Small introduction text about me.</p>
-          <p className="yellow-font fs-1rem-to-1-25rem">
-            Small introduction text about me. Small introduction text about me. Small introduction text about me.Small introduction text about me. Small introduction text about me. Small introduction
-            text about me.Small introduction text about me. Small introduction text about me. Small introduction text about me.
-          </p>
+          <p className="yellow-font fs-1rem-to-1-25rem">As a passionate front-end developer and designer, I love combining creativity and functionality to craft clever, user-friendly websites. Based in Valdres, Norway, my work is often inspired by the stunning nature surrounding me.</p>
+          <p className="yellow-font fs-1rem-to-1-25rem">With a fresh education in web development and design, I bring a modern, responsive approach to every project. I focus on clean, intuitive interfaces that enhance user experience, while incorporating unique design elements drawn from the world around me. I’m excited to create innovative digital solutions that leave a lasting impression..</p>
         </div>
       </div>
     </section>
